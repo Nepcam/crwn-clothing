6e2af00665a3828e7b0ace30aba286a48d924888
Nepcam <cameron.nepe@gmail.com>
2019-11-01 12:21:16 +1300

    ShopPage and CollectionPreview components

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -4,12 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
@@ -18,7 +13,7 @@ function App() {
           {/* A <Switch> looks through its children <Route>s and
                 renders the first one that matches the current URL. */}
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
