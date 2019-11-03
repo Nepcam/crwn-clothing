85725e4c41197d4a7a95c065d1f6685cc00979c8
Nepcam <cameron.nepe@gmail.com>
2019-11-04 12:07:40 +1300

    Sign in component

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-andsign-up/sign-in-andsign-up.component' 
 import Header from './components/header/header.component';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
                   renders the first one that matches the current URL. */}
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
     </div>
   );
