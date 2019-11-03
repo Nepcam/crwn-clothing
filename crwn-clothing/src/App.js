0ab7fc7b8a55edda6448001e60a2f555e274d3ff
Nepcam <cameron.nepe@gmail.com>
2019-11-03 21:38:46 +1300

    working on the header component jsx and scss

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -5,16 +5,18 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
-      <Switch>
-          {/* A <Switch> looks through its children <Route>s and
-                renders the first one that matches the current URL. */}
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-      </Switch>
+      <Header />
+        <Switch>
+            {/* A <Switch> looks through its children <Route>s and
+                  renders the first one that matches the current URL. */}
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+        </Switch>
     </div>
   );
 }
