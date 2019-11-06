0dba27fed18b942656fc1f857b95bd073c199ae9
Nepcam <cameron.nepe@gmail.com>
2019-11-06 15:21:17 +1300

    sign in and out authentication

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -7,20 +7,45 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-andsign-up/sign-in-andsign-up.component' 
 import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header />
-        <Switch>
-            {/* A <Switch> looks through its children <Route>s and
-                  renders the first one that matches the current URL. */}
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
-        </Switch>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribedFromAuth = null
+
+  componentDidMount() {
+    this.unsubscribedFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribedFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+          <Switch>
+              {/* A <Switch> looks through its children <Route>s and
+                    renders the first one that matches the current URL. */}
+            <Route exact path='/' component={HomePage} />
+            <Route path='/shop' component={ShopPage} />
+            <Route path='/signin' component={SignInAndSignUpPage} />
+          </Switch>
+      </div>
+    );
+  }
+};
 
 export default App;
