cd91d36f800d02cad74ed52e1f542c1b95e004f6
Nepcam <cameron.nepe@gmail.com>
2019-11-15 12:26:54 +1300

    setting up Redux

--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'; 
+import { Provider } from 'react-redux'
 
 // BrowserRouter is a component - gives our application all of the functionality of routing
 
@@ -8,9 +9,11 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>, 
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>, 
   document.getElementById('root')
 );
 
