2f9d866ee54014fdd3d9f5bc001bdacf5a0c5efc
Nepcam <cameron.nepe@gmail.com>
2019-11-20 21:56:01 +1300

    taking notes on redux concepts

--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -10,6 +10,8 @@ import App from './App';
 
 import store from './redux/store';
 
+// the <provider /> is this component class that we get from reactor redux that once passed the store object we'll be able to give that read store context to the rest of the application so we can dispatch actions
+
 ReactDOM.render(
   <Provider store={store}> 
   
--- crwn-clothing/src/redux/user/user.actions.js
+++ crwn-clothing/src/redux/user/user.actions.js
@@ -1,4 +1,4 @@
 export const setCurrentUser = user =>({
-  type: 'SET_CURRENT_USER',
+  type: 'SET_CURRENT_USER',   // this is the string that our user.reducer is expecting 
   payload: user
 });
