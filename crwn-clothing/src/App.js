3fe29e633b4c0910f22f45933a7353679e3d9f66
Nepcam <cameron.nepe@gmail.com>
2019-11-24 21:42:58 +1300

    updating our app to redirect to home if user is signed in

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -46,12 +46,22 @@ class App extends React.Component {
                     renders the first one that matches the current URL. */}
             <Route exact path='/' component={HomePage} />
             <Route path='/shop' component={ShopPage} />
-            <Route path='/signin' component={SignInAndSignUpPage} />
+            <Route 
+              exact path='/signin'
+              render={() => 
+                this.props.currentUser ? 
+                (<Redirect to='/' />) : 
+                (<SignInAndSignUpPage/>)} 
+              />
           </Switch>
       </div>
     );
   }
 };
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
--- crwn-clothing/src/redux/user/user.actions.js
+++ crwn-clothing/src/redux/user/user.actions.js
@@ -1,4 +1,6 @@
+import { UserActionTypes } from './user.types';
+
 export const setCurrentUser = user =>({
-  type: 'SET_CURRENT_USER',   // this is the string that our user.reducer is expecting 
+  type: UserActionTypes.SET_CURRENT_USER,   // this is the string that our user.reducer is expecting 
   payload: user
 });
