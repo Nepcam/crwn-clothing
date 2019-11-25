2890c1bbf73f8684fe769c5d332f1e2ae0e52e86
Nepcam <cameron.nepe@gmail.com>
2019-11-26 10:03:59 +1300

    added cartIcon and cartDropdown components to our application, also implemented cart reducer

--- crwn-clothing/src/redux/root-reducer.js
+++ crwn-clothing/src/redux/root-reducer.js
@@ -3,9 +3,11 @@
 import { combineReducers } from 'redux';
 
 import userReducer from './user/user.reducer';
+import cartReducer from './cart/cart.reducer';
 
 export default combineReducers({
-  user: userReducer
+  user: userReducer,
+  cart: cartReducer
 });
 
 // We are pulling the userReducer into this combinedReducers which will then return it into one giant object that is appropriately bound with all the redux functionality that we want
