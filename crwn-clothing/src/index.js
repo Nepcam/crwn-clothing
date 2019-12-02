8fb0ef868b4f3ef534f201d25822f16e36b40a71
Nepcam <cameron.nepe@gmail.com>
2019-12-02 15:41:44 +1300

    redux persist

--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'; 
 import { Provider } from 'react-redux' // a component that we get from react-redux store, the provider componnet has to be the parent of everything
-
+import { PersistGate } from 'redux-persist/integration/react';
 // BrowserRouter is a component - gives our application all of the functionality of routing
 
 import './index.css';
 import App from './App';
 
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 // the <provider /> is this component class that we get from reactor redux that once passed the store object we'll be able to give that read store context to the rest of the application so we can dispatch actions
 
@@ -16,7 +16,9 @@ ReactDOM.render(
   <Provider store={store}> 
   
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>, 
   document.getElementById('root')
--- crwn-clothing/src/redux/root-reducer.js
+++ crwn-clothing/src/redux/root-reducer.js
@@ -1,13 +1,23 @@
 // This code combines all the other states together
 
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
 });
 
 // We are pulling the userReducer into this combinedReducers which will then return it into one giant object that is appropriately bound with all the redux functionality that we want
+
+export default persistReducer(persistConfig, rootReducer);
