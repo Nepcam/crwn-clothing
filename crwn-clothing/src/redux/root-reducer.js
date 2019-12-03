6d1d5a0882f05a4db0d75b3db6a14be8101ec7ba
Nepcam <cameron.nepe@gmail.com>
2019-12-03 13:40:59 +1300

    directory and collection state into redux

--- crwn-clothing/src/redux/root-reducer.js
+++ crwn-clothing/src/redux/root-reducer.js
@@ -6,6 +6,8 @@ import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 
 const persistConfig = {
   key: 'root',
@@ -15,7 +17,9 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer
 });
 
 // We are pulling the userReducer into this combinedReducers which will then return it into one giant object that is appropriately bound with all the redux functionality that we want
