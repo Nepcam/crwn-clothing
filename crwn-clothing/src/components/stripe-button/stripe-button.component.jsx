85acb92940d785633e20e1ce0e009b7502ba207d
Nepcam <cameron.nepe@gmail.com>
2019-12-05 12:19:42 +1300

    implement stripe for payment gateway

---
+++ crwn-clothing/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_ipwvkZ7t4MH3kdfo8v9auMni00Ii3DasyS'
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  }
+
+  return (
+    <StripeCheckout 
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      imagedescription={`Your total os $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
