3b5f61274bbf702fbcbfd146c3b5662cdd78e48a
Nepcam <cameron.nepe@gmail.com>
2020-02-13 13:38:11 +1300

    firebase tidy ups

--- crwn-clothing/src/firebase/firebase.utils.js
+++ crwn-clothing/src/firebase/firebase.utils.js
@@ -42,7 +42,10 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 } 
 
-export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
   const collectionRef = firestore.collection(collectionKey);
 
   const batch = firestore.batch();
@@ -54,7 +57,7 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
   return await batch.commit();
 };
 
-export const convertCollectionsSnapshotToMap = (collections) => {
+export const convertCollectionsSnapshotToMap = collections => {
   const transformedCollection = collections.docs.map(doc => {
     const { title, items } = doc.data();
 
@@ -63,14 +66,14 @@ export const convertCollectionsSnapshotToMap = (collections) => {
       id: doc.id,
       title,
       items
-    }
+    };
   });
 
-  return transformedCollection.reduce((accumulator, collection) =>  {
+  return transformedCollection.reduce((accumulator, collection) => {
     accumulator[collection.title.toLowerCase()] = collection;
     return accumulator;
-  } , {});
-}
+  }, {});
+};
 
 firebase.initializeApp(config);
 export const auth = firebase.auth();
